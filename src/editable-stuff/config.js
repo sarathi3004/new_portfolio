f0129d42ad7a9523b83a9e139cc5e36e0ed9caa0
UVARAJAN04 <sabari2005clg@gmail.com>
2025-02-16 20:49:50 +0530

    first commit

---
+++ src/editable-stuff/config.js
@@ -0,0 +1,155 @@
+// Navigation Bar SECTION
+const navBar = {
+  show: true,
+};
+
+// Main Body SECTION
+const mainBody = {
+  gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
+  firstName: "Sarathi",
+  middleName: "",
+  lastName: "S",
+  message: " Passionate about changing the world with technology. ",
+  icons: [
+    {
+      image: "fa-github",
+      url: "https://github.com/sarathi3004/",
+    },
+    // {
+    //   image: "fa-facebook",
+    //   url: "https://www.facebook.com/hashirshoaeb",
+    // },
+    // {
+    //   image: "fa-instagram",
+    //   url: "https://www.instagram.com/hashirshoaeb/",
+    // },
+    {
+      image: "fa-linkedin",
+      url: "https://www.linkedin.com/in/sarathi-senthil/",
+    },
+    // {
+    //   image: "fa-twitter",
+    //   url: "https://www.twitter.com/hashirshoaeb/",
+    // },
+  ],
+};
+
+// ABOUT SECTION
+// If you want the About Section to show a profile picture you can fill the profilePictureLink either with:
+//a) your Instagram username
+//      i.e:profilePictureLink:"johnDoe123",
+//b) a link to an hosted image
+//      i.e:profilePictureLink:"www.picturesonline.com/johnDoeFancyAvatar.jpg",
+//c) image in "editable-stuff" directory and use require("") to import here,
+//      i.e: profilePictureLink: require("../editable-stuff/hashirshoaeb.png"),
+//d) If you do not want any picture to be displayed, just leave it empty :)
+//      i.e: profilePictureLink: "",
+// For Resume either provide link to your resume or import from "editable-stuff" directory
+//     i.e resume: require("../editable-stuff/resume.pdf"),
+//         resume: "https://docs.google.com/document/d/13_PWdhThMr6roxb-UFiJj4YAFOj8e_bv3Vx9UHQdyBQ/edit?usp=sharing",
+
+const about = {
+  show: true,
+  heading: "About Me",
+  imageLink: require("../editable-stuff/sarathi.jpg"),
+  imageSize: 375,
+  message:
+    "My name is Sarathi S. I am currently pursuing a Bachelor of Technology in Computer Science and Engineering (B.Tech CSE) at MVIT, Puducherry, as part of the 2026 batch, with a CGPA of 8.45. I am most passionate about applying my technical skills to solve real-world problems and giving back to the community through innovative projects.In my free time, I work on projects like a real-time deep learning system for product detection and a food tracker and donor platform, focusing on automation and AI-driven solutions. My goal is to continue growing in areas like cybersecurity and DBMS while contributing to impactful advancements in software engineering.",
+  resume: "https://drive.google.com/file/d/1QhIQ5IIJm3eeyp6jtC8-vcZ4zfiS5nZB/view?usp=sharing",
+};
+
+// PROJECTS SECTION
+// Setting up project lenght will automatically fetch your that number of recently updated projects, or you can set this field 0 to show none.
+//      i.e: reposLength: 0,
+// If you want to display specfic projects, add the repository names,
+//      i.e ["repository-1", "repo-2"]
+const repos = {
+  show: true,
+  heading: "Sarathi S",
+  gitHubUsername: "sarathi3004", //i.e."johnDoe12Gh"
+  reposLength: 4,
+  specificRepos: [],
+};
+
+// Leadership SECTION
+const leadership = {
+  show: false,
+  heading: "Leadership",
+  message:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
+  images: [
+    { 
+      img: require("../editable-stuff/sarathi.jpg"), 
+      label: "First slide label", 
+      paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
+    },
+    { 
+      img: require("../editable-stuff/sarathi.jpg"), 
+      label: "Second slide label", 
+      paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
+    },
+  ],
+  imageSize: {
+    width:"615",
+    height:"450"
+  }
+};
+
+// SKILLS SECTION
+const skills = {
+  show: true,
+  heading: "Skills",
+  hardSkills: [
+    { name: "Python", value: 90 },
+    { name: "SQL", value: 75 },
+    // { name: "Data Structures", value: 85 },
+    { name: "C/C++", value: 70 },
+    // { name: "JavaScript", value: 90 },
+    // { name: "React", value: 65 },
+    { name: "HTML/CSS", value: 80 },
+    // { name: "C#", value: 80 },
+  ],
+  softSkills: [
+    { name: "Goal-Oriented", value: 80 },
+    { name: "Collaboration", value: 90 },
+    { name: "Positivity", value: 75 },
+    { name: "Adaptability", value: 85 },
+    { name: "Problem Solving", value: 75 },
+    { name: "Empathy", value: 90 },
+    { name: "Organization", value: 70 },
+    { name: "Creativity", value: 90 },
+  ],
+};
+
+// GET IN TOUCH SECTION
+const getInTouch = {
+  show: true,
+  heading: "Get In Touch",
+  message:
+    "I'm currently looking for full-time Software Engineering or Machine Learning opportunities! If you know of any positions available, if you have any questions, or if you just want to say hi, please feel free to email me at",
+  email: "sarathisenthil17@gmail.com",
+};
+
+const experiences = {
+  show: false,
+  heading: "Experiences",
+  data: [
+    {
+      role: 'Software Engineer',// Here Add Company Name
+      companylogo: require('../assets/img/dell.png'),
+      date: 'June 2018 – Present',
+    },
+    {
+      role: 'Front-End Developer',
+      companylogo: require('../assets/img/boeing.png'),
+      date: 'May 2017 – May 2018',
+    },
+  ]
+}
+
+// Blog SECTION
+// const blog = {
+//   show: false,
+// };
+
+export { navBar, mainBody, about, repos, skills, leadership, getInTouch, experiences };
